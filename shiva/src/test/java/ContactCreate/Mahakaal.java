b994c1772e7e7a9547e631830c325a24387368a9
SIBANI SHANKAR HOTA <SIBANI SHANKAR HOTA@LAPTOP-90E9IVHD>
2025-02-08 10:57:32 +0530

    kuch v

--- shiva/src/test/java/ContactCreate/Mahakaal.java
+++ shiva/src/test/java/ContactCreate/Mahakaal.java
@@ -4,7 +4,7 @@ public class Mahakaal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+         System.out.println("kal raat ko daru pike kutto ko biscuits dia");
 	}
 
 }
---
+++ shiva/src/test/java/hota/yourte.java
@@ -0,0 +1,10 @@
+package hota;
+
+public class yourte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("ghfjjfj");
+	}
+
+}
